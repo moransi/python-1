eb7dd4542ed01902828450a587bd2e4399e8ffca
nadergan <nadergan@gmail.com>
2017-12-22 00:09:04 +0200

    ignore case

--- translate.py
+++ translate.py
@@ -3,7 +3,8 @@
 word = input("Please enter a word to translate: ")
 
 for d in open('dictionary.txt', 'r').readlines():
-    if d.startswith(f"{word}"):
+    word_ucase = word.upper()
+    if d.upper().startswith(f"{word_ucase}"):
         english = d.split(':')[0]
         translated = d.split(':')[1].strip()
         print(f"{english} is {translated} in German")
