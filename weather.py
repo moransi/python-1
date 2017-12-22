db0abdcaf77678abddc76b73ae30d4140411b36b
nadergan <nadergan@gmail.com>
2017-12-22 02:17:37 -0800

    Add comment with pip path

--- weather.py
+++ weather.py
@@ -1,5 +1,8 @@
 import requests
 
+## /c/Users/mcse/AppData/Local/Programs/Python/Python36-32/Scripts/pip
+
+
 resp = requests.get('https://api.darksky.net/forecast/0d584daf877a4ff2998afe4329840ef9/32.7996897,34.9817565')
 if resp.status_code != 200:
     # This means something went wrong.
