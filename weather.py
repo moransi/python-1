25a74f776395b3decfacf7af8cd70e1dd3691bc1
nadergan <nadergan@gmail.com>
2017-12-22 12:00:32 +0200

    Add weather.py

---
+++ weather.py
@@ -0,0 +1,11 @@
+import requests
+
+resp = requests.get('https://api.darksky.net/forecast/0d584daf877a4ff2998afe4329840ef9/32.7996897,34.9817565')
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise requests.RequestException('GET ERROR {}'.format(resp.status_code))
+
+j_resp = resp.json()
+
+print(resp.json())
+
