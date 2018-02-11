625aeb8ca92f5ebe4927d1e7d322c5bc9c4ba15a
nadergan <nadergan@gmail.com>
2018-02-11 19:47:51 +0200

    Add googlemaps

--- weather.py
+++ weather.py
@@ -3,12 +3,30 @@ import requests
 ## /c/Users/mcse/AppData/Local/Programs/Python/Python36-32/Scripts/pip
 ## http://maps.googleapis.com/maps/api/geocode/json?address=haifa
 
-resp = requests.get('https://api.darksky.net/forecast/0d584daf877a4ff2998afe4329840ef9/32.7996897,34.9817565')
+resp = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=haifa')
+
 if resp.status_code != 200:
     # This means something went wrong.
     raise requests.RequestException('GET ERROR {}'.format(resp.status_code))
 
 j_resp = resp.json()
+print(j_resp)
 
-print(resp.json())
+## results ---> list item #0 ---> geometry ---> location
+
+results = j_resp['results'][0]
+location = results['geometry']['location']
+lat = location['lat']
+lng = location['lng']
+
+darksky_url = f"https://api.darksky.net/forecast/0d584daf877a4ff2998afe4329840ef9/{lat},{lng}"
+
+# resp = requests.get('https://api.darksky.net/forecast/0d584daf877a4ff2998afe4329840ef9/32.7996897,34.9817565')
+# if resp.status_code != 200:
+#     # This means something went wrong.
+#     raise requests.RequestException('GET ERROR {}'.format(resp.status_code))
+#
+# j_resp = resp.json()
+
+# print(resp.json())
 
